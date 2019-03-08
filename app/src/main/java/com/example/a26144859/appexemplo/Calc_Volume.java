485ee7f7b5d2af9cff15a90b8c25711616868cb8
rosahanne <emanuelle.rh@gmail.com>
2019-03-08 19:39:37 -0300

    Initial commit

---
+++ app/src/main/java/com/example/a26144859/appexemplo/Calc_Volume.java
@@ -0,0 +1,37 @@
+package com.example.a26144859.appexemplo;
+
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Calc_Volume extends AppCompatActivity {
+
+    public static final double CONSTANTE = 3.141559;
+    private TextInputEditText altura;
+    private TextInputEditText raio;
+    private Button btnCalcular;
+    private TextView resultado;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc__volume);
+
+        altura = findViewById(R.id.inp_Altura);
+        raio = findViewById(R.id.inp_Raio);
+        btnCalcular = findViewById(R.id.btn_Calcular);
+        resultado = findViewById(R.id.txtResult);
+    }
+
+    public void calcular(View v){
+        double vlAltura = Double.parseDouble(altura.getText().toString());
+        double vlRaio = Double.parseDouble(raio.getText().toString());
+        double volume = CONSTANTE * (vlRaio * vlRaio) * vlAltura;
+        String texto = "Valor do volume é: " + volume;
+        resultado.setText(texto);
+    }
+}
---
+++ app/src/main/java/com/example/a26144859/appexemplo/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.a26144859.appexemplo;
+
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mainBtn;
+    private TextInputEditText vlConta;
+    private TextView resultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mainBtn = findViewById(R. id.btnCalcular);
+        vlConta = findViewById(R. id.inputValor);
+        resultado = findViewById(R. id.txtResultado);
+
+    }
+
+    public void calcular(View v){
+        double conta = Double.parseDouble(vlConta.getText().toString());
+        double res = conta * 1.1;
+        String texto = "Valor da conta é: " + res;
+        resultado.setText(texto);
+    }
+
+}
